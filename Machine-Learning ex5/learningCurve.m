fb545771bcc30a0a869f9f7632f75f5a4e2cce5a
RadutRares <jonsnowofnightswatch1@gmail.com>
2017-05-17 11:19:53 -0400

    Completed 5th assignment

---
+++ Machine-Learning ex5/learningCurve.m
@@ -0,0 +1,47 @@
+function [error_train, error_val] = ...
+    learningCurve(X, y, Xval, yval, lambda)
+%LEARNINGCURVE Generates the train and cross validation set errors needed 
+%to plot a learning curve
+%   [error_train, error_val] = ...
+%       LEARNINGCURVE(X, y, Xval, yval, lambda) returns the train and
+%       cross validation set errors for a learning curve. In particular, 
+%       it returns two vectors of the same length - error_train and 
+%       error_val. Then, error_train(i) contains the training error for
+%       i examples (and similarly for error_val(i)).
+%
+%   In this function, you will compute the train and test errors for
+%   dataset sizes from 1 up to m. In practice, when working with larger
+%   datasets, you might want to do this in larger intervals.
+%
+
+% Number of training examples
+m = size(X, 1);
+
+error_train = zeros(m, 1);
+error_val   = zeros(m, 1);
+
+for i = 1:m 
+  Xtrain = X(1:i,:);
+  ytrain = y(1:i);
+  % Train Subset
+  theta = trainLinearReg(Xtrain, ytrain, lambda);
+  % Error
+  % error_train(i) = (1/(2*i)) * (Xtrain * theta - ytrain)' * (Xtrain * theta - ytrain);
+  error_train(i) = linearRegCostFunction(Xtrain, ytrain, theta, 0);
+  % Validation Error
+  % error_val(i) = (1/(2*i)) * (Xval * theta - yval)' * (Xval * theta - yval);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
+
+
+
+
+
+
+
+
+% -------------------------------------------------------------
+
+% =========================================================================
+
+end
